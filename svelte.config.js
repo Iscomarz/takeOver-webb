281b75726617d3be7a7589b125ba31393f4be994
Fancisco Martinez <francisco.martinez@bluewebsoft.com>
2024-09-27 11:58:25 -0600

    se ajusta conf svelte prerender

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({status, path, referrer, referenceType }) =>{
+				if(status === 404){
+					console.warn(`404 error on prerendering: ${path}`);
+					return;
+				}
+				throw new Error();
+			}
+		}
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(`${status} ${path} (linked from ${referrer})`)
 };
 
 export default config;
