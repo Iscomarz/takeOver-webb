afe5c81ec0ee5d1b98f66ff0b8d525e963115bb0
Fancisco Martinez <francisco.martinez@bluewebsoft.com>
2025-02-19 10:19:52 -0600

    animacion loading y no perder datos al regresar de checkout

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -6,14 +6,24 @@
   import Tickets from "../components/Tickets.svelte";
   import supabase from "../../lib/supabase";
   import { onMount, tick } from "svelte";
+  import { invalidateAll } from "$app/navigation";
+  import { tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
+  import logo from "$lib/images/takeover-logo.png";
+  import { fade } from 'svelte/transition';
+
+  let scale = tweened(1, {
+    duration: 400,
+    easing: cubicOut,
+  });
+  let loading = true;
 
   let mEvento = {};
   let fases = [];
   let productosStripe = [];
   let urlImagenPortada;
 
-  onMount(async () => {
-    //Obtener evento activo
+  async function loadData() {
     let { data: evento, error } = await supabase
       .from("mEvento")
       .select("*")
@@ -25,6 +35,12 @@
     if (error) {
       console.log("Error al traer el evento activo");
     } else {
+      //Traer portada del evento
+      const { data } = supabase.storage
+        .from("imageEventos")
+        .getPublicUrl(mEvento.pathImage);
+
+      urlImagenPortada = data.publicUrl;
       //Obtener las faces o tickets del evento selecionado
       let { data: cFases, errorF } = await supabase
         .from("cFaseEvento")
@@ -34,28 +50,37 @@
       if (cFases.length > 0) {
         fases = cFases;
         // Llama a la función para obtener los productos
-        productosStripe = await getProducts();
+        //productosStripe = await getProducts();
         //enlazar identificador de precio con fase (Despues hay que hacerlo directamente al crear la fase en supabase y el producto en stripe)
-        for(const producto of productosStripe){
-          for(const fase of fases){
-            if(producto.name == fase.nombreFace){
-              fase.idPrecioStripe = producto.default_price;
-            }
-          }
-        }
-
+        //for(const producto of productosStripe){
+        //  for(const fase of fases){
+        //    if(producto.name == fase.nombreFace){
+        //      fase.idPrecioStripe = producto.default_price;
+        //    }
+        //  }
+        //}
       } else if (errorF) {
         console.log("Error al traer las fases");
       }
-      //Traer portada del evento
-      const { data } = supabase.storage
-        .from("imageEventos")
-        .getPublicUrl(mEvento.pathImage);
-
-      urlImagenPortada = data.publicUrl;
     }
-
     await tick();
+    loading = false;
+  }
+
+  onMount(async () => {
+    invalidateAll();
+    //Obtener evento activo
+    await loadData();
+    let interval = setInterval(() => {
+      if (loading) {
+        scale.set(1.1);
+        setTimeout(() => {
+          scale.set(1);
+        }, 400);
+      } else {
+        clearInterval(interval);
+      }
+    }, 1500);
   });
 
   async function getProducts() {
@@ -86,37 +111,57 @@
   <meta name="description" content="About this app" />
 </svelte:head>
 
-{#if urlImagenPortada}
-  <div class="img-event">
-    <span>
-      <img src={urlImagenPortada} alt="portada" />
-    </span>
+{#if loading}
+  <div class="loading-container" transition:fade={{ duration: 200 }}>
+    <img
+      src={logo}
+      style="transform: scale({$scale})"
+      alt="loading"
+    />
   </div>
+{:else}
+  {#if urlImagenPortada}
+    <div class="img-event">
+      <span>
+        <img src={urlImagenPortada} alt="portada" />
+      </span>
+    </div>
+  {/if}
+
+  <section class="info-event-short">
+    <Title titulo={mEvento.nombreEvento} fecha="Domingo, 16 de Marzo 2025" />
+  </section>
+
+  <section class="info-event">
+    <div class="components">
+      <Date fecha="Dom, 16 Mar 2025 17:00 - 2:00" />
+      <Location
+        nombreLugar={mEvento.venue}
+        direccion={mEvento.direccion}
+        linkMaps={mEvento.direccionURL}
+      />
+      <AboutEvent descripcion={mEvento.descripcion} />
+      {#if fases.length > 0}
+        <Tickets ticketDataEve={fases} />
+      {/if}
+    </div>
+  </section>
 {/if}
 
-<section class="info-event-short">
-  <Title
-    titulo={mEvento.nombreEvento}
-    fecha="Domingo, 16 de Marzo 2025"
-  />
-</section>
-
-<section class="info-event">
-  <div class="components">
-    <Date fecha="Dom, 16 Mar 2025 17:00 - 2:00" />
-    <Location
-      nombreLugar={mEvento.venue}
-      direccion={mEvento.direccion}
-      linkMaps={mEvento.direccionURL}
-    />
-    <AboutEvent descripcion={mEvento.descripcion} />
-    {#if fases.length > 0}
-      <Tickets ticketDataEve={fases} />
-    {/if}
-  </div>
-</section>
-
 <style>
+  .loading-container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .loading-container img {
+    width: 150px;
+    transition: transform 0.75s ease-in-out;
+  }
   .img-event {
     padding: 20px;
     width: 100%;
