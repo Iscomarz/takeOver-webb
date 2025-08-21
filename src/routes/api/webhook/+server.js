ac21f21db5d7514e496bf44bafa7d80adce04a25
Fancisco Martinez <francisco.martinez@bluewebsoft.com>
2025-08-21 11:18:55 -0600

    Faltaba un await

--- src/routes/api/webhook/+server.js
+++ src/routes/api/webhook/+server.js
@@ -124,7 +124,7 @@ async function capturarCheckOut(sessionCheckout, stripeEventId, event, paymentIn
 
   if(amount == 0 && cantidadT != 0){
     console.log("Entro a acreditar y generar tickets")
-    acreditaPagoYGeneraTickets(event, stripeEventId);
+    await acreditaPagoYGeneraTickets(event, stripeEventId);
   }
 
   if (error) {
